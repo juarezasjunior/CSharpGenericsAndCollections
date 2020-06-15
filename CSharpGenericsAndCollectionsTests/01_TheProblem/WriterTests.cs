c0394d4e39f887b3c5cdeecb4b86d931ca86da51
Juarez Júnior <juarez.a.s.junior@gmail.com>
2020-06-15 11:51:58 -0300

    Alterações.

--- CSharpGenericsAndCollectionsTests/01_TheProblem/WriterTests.cs
+++ CSharpGenericsAndCollectionsTests/01_TheProblem/WriterTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CSharpGenericsAndCollections._TheProblem;
 using NUnit.Framework;
 
 namespace CSharpGenericsAndCollectionsTests._TheProblem
@@ -6,9 +7,31 @@ namespace CSharpGenericsAndCollectionsTests._TheProblem
     [TestFixture]
     public class WriterTests
     {
-        public void WritingTest()
+        [Test]
+        public void WriterTest()
         {
+            var writer = new Writer();
 
+            // I need a method for each type that I want to write
+            writer.Write(true);
+            writer.Write("teste");
+            writer.Write(1);
+            writer.Write(10.5);
+
+            var myNewObject = new MyNewObject();
+
+            // I cannot do this, because my method don't has a parameter of this type
+            //writer.Write(myNewObject);
+
+            Assert.That(writer.LogLines.Count, Is.EqualTo(4));
+        }
+
+        public class MyNewObject
+        {
+            public override string ToString()
+            {
+                return "This is my new object";
+            }
         }
     }
 }
