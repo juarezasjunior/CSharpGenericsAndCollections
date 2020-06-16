b20a31874061fa55fdb8dc5e28aa112df780765a
Juarez Júnior <juarez.a.s.junior@gmail.com>
2020-06-16 14:34:06 -0300

    Alterações.

--- CSharpGenericsAndCollections/03_Generics/Repository.cs
+++ CSharpGenericsAndCollections/03_Generics/Repository.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace CSharpGenericsAndCollections._Generics
 {
-    public class Repository<T> where T : BaseModel
+    public class Repository<T> where T : BaseModel, new()
     {
         private List<T> database = new List<T>();
 
